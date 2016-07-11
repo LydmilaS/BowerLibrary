7d7be704386d7ae2a3e11ac6d1bbc5aee55fcab9
Luda Sukhomudrenko <lydmilazoloto@gmail.com>
2016-07-11 13:28:29 +0300

    basic functionality added

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,8 +1,8 @@
 /**
  * Created by luda on 09.07.16.
  */
-var app = angular.module("myApp", []);
+var app = angular.module("myApp", ['dndLists', 'ngContentEditable']);
 app.controller('mainController', ['$scope', function ($scope) {
     $scope.columns = getColumns();
-    
+    $scope.trash = getTrash();
 }]);
--- main.js
+++ main.js
@@ -6,28 +6,43 @@ function Sheet(title, content, time) {
     this.content = content;
     this.time = time;
     this.sheetEditable = false;
-    
 }
 
 function Column(title) {
     this.title = title;
     this.sheets = [];
     this.editable = false;
+    this.visibleSheets = true;
     this.addSheet = function(title, content, time) {
         this.sheets.push(new Sheet(title, content, time));
     };
+
+    this.toggleVisibleSheets = function () {
+        this.visibleSheets = !this.visibleSheets;
+    };
+
+    this.removeAllSheets = function () {
+        var confirmDelete = window.confirm('Remove all?');
+        if(confirmDelete){
+            this.sheets = [];
+        }
+    }
 }
 
 var getColumns = function () {
-    var colWithSheets = new Column('In Progress', 'in progress', 88);
-    colWithSheets.addSheet('first list title', 'first list content');
-    colWithSheets.addSheet('second list title', 'second list content');
+    var colWithSheets = new Column('In Progress');
+    colWithSheets.addSheet('first list title', 'first list content', 25);
+    colWithSheets.addSheet('second list title', 'second list content', 87);
     colWithSheets.editable = true;
 
     return [
         colWithSheets
-        , new Column('Ready To Test', 'ready to test', 14)
-        , new Column('Tested', 'tested', 67)
+        , new Column('Ready To Test')
+        , new Column('Tested')
     ];
 };
 
+var getTrash = function () {
+    var trash = new Column('Trash');
+    return trash;
+};
